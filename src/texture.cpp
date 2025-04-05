a12fedb52161807598a0d7ae057f98fbd407a239
Kevin Hecke <keheck@web.de>
2025-04-05 23:08:12 +0200

    Fix missing texture weirdness

--- src/texture.cpp
+++ src/texture.cpp
@@ -3,17 +3,10 @@
 #include <glad/glad.h>
 #include <iostream>
 
-unsigned char missingTexture4x4[] {
-    0x00, 0x00, 0x00,  0x00, 0x00, 0x00,    0xFF, 0x00, 0xFF,  0xFF, 0x00, 0xFF, 
-    0x00, 0x00, 0x00,  0x00, 0x00, 0x00,    0xFF, 0x00, 0xFF,  0xFF, 0x00, 0xFF, 
 
-    0xFF, 0x00, 0xFF,  0xFF, 0x00, 0xFF,    0x00, 0x00, 0x00,  0x00, 0x00, 0x00, 
-    0xFF, 0x00, 0xFF,  0xFF, 0x00, 0xFF,    0x00, 0x00, 0x00,  0x00, 0x00, 0x00,
-};
-
-unsigned char missingTexture2x2[] {
-    0x00, 0x00, 0x00,    0xFF, 0x00, 0xFF,
-    0xFF, 0x00, 0xFF,    0x00, 0x00, 0x00
+unsigned char missingTexture[] {
+    0x00, 0x00, 0x00,    0xFF, 0x00, 0xFF, 0x00, 0x00,
+    0xFF, 0x00, 0xFF,    0x00, 0x00, 0x00, 0x00, 0x00
 };
 
 const int channelsToFormat[] {
@@ -33,15 +26,11 @@ Texture::Texture(const char *fileName) {
 
     if(data) {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, channelsToFormat[numChannels], GL_UNSIGNED_BYTE, data);
-        // glGenerateMipmap(GL_TEXTURE_2D);
+        glGenerateMipmap(GL_TEXTURE_2D);
     }
     else {
         std::cerr << "Failed to read texture " << fileName << ", using missing texture instead" << std::endl;
-        #ifdef USE2X2
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 2, 2, 0, GL_RGB, GL_UNSIGNED_BYTE, missingTexture2x2);
-        #else
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 4, 4, 0, GL_RGB, GL_UNSIGNED_BYTE, missingTexture4x4);
-        #endif
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 2, 2, 0, GL_RGB, GL_UNSIGNED_BYTE, missingTexture);
     }
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
