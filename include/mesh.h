2f1a10d0fde87349edd81eaf70461a0e1560ae3c
Kevin Hecke <keheck@web.de>
2025-04-18 00:43:25 +0200

    Refactored Constructor to take Vertex struct vectors

--- include/mesh.h
+++ include/mesh.h
@@ -1,14 +1,28 @@
 #pragma once
 
 #include <vector>
+#include <map>
+#include <string>
 #include <glad/glad.h>
+#include <glm/glm.hpp>
+
+#include "shader.h"
+#include "texture.h"
 
 class Mesh;
+struct Vertex;
 struct VertexDataDescriptor;
+
+struct Vertex {
+    glm::vec3 position;
+    glm::vec3 normal;
+    glm::vec2 uv;
+};
 
 class Mesh {
     public:
         Mesh(std::vector<float> &data, std::vector<unsigned int> &indices);
+        Mesh(std::vector<Vertex> &vertices, std::vector<unsigned int> &indices);
         ~Mesh();
         void cleanup();
         void bindArray();
