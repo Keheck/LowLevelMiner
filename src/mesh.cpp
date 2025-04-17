6691ed1325fa32f3a6358aa8743863f3186832e2
Kevin Hecke <keheck@web.de>
2025-04-18 01:59:33 +0200

    Use Vertex struct for mesh building

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -2,14 +2,22 @@
 #include <stdarg.h>
 #include <utility>
 
-Mesh::Mesh(std::vector<float> &vertexData, std::vector<unsigned int> &indices) {
+Mesh::Mesh(std::vector<Vertex> &vertexData, std::vector<unsigned int> &indices) {
     glGenVertexArrays(1, &(this->VAO));
     glGenBuffers(1, &(this->VBO));
     glGenBuffers(1, &(this->EBO));
 
     glBindVertexArray(this->VAO);
     glBindBuffer(GL_ARRAY_BUFFER, this->VBO);
-    glBufferData(GL_ARRAY_BUFFER, vertexData.size()*sizeof(float), vertexData.data(), GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, vertexData.size()*sizeof(Vertex), vertexData.data(), GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(offsetof(Vertex, position)));
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(offsetof(Vertex, uv)));
+    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(offsetof(Vertex, normal)));
+
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    glEnableVertexAttribArray(2);
     
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size()*sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
